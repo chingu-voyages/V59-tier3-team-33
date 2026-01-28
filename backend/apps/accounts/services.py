f1aa619c5bdc237c24152b7cde2dc567978606b4
Direwen <direwensik@gmail.com>
2026-01-28 11:31:54 +0630

    fix: replace user receiver with the actual complete signup signal from allauth

--- backend/apps/accounts/services.py
+++ backend/apps/accounts/services.py
@@ -1,14 +1,14 @@
 from django.conf import settings
-from django.contrib.auth.forms import User
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 def send_welcome_mail(user: User):
+    name_to_use = user.first_name if user.first_name else "Traveller"
     subject = "Welcome to JoyRoute"
     message = (
-        f"Hi {user.first_name},\n\n"
+        f"Hi {name_to_use},\n\n"
         "Thank you for joining JoyRoute! We're excited to have you on board.\n\n"
         "Best regards,\n"
         "JoyRoute Team"
--- backend/apps/accounts/signals.py
+++ backend/apps/accounts/signals.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 from .services import send_welcome_mail
+from allauth.account.signals import user_signed_up
 
 User = get_user_model()
 
-@receiver(post_save, sender=User)
-def send_welcome_email(sender, instance, created, **kwargs):
+@receiver(user_signed_up)
+def send_welcome_email(sender, request, user, **kwargs):
     # send a welcome email to the user when the user is created
-    if created:
-        send_welcome_mail(instance)
+    send_welcome_mail(user)
